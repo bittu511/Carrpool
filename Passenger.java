fb487c13e82c1b03da9d4d3ee894e9a212d2471e
Raj <30487563+bittu511@users.noreply.github.com>
2018-04-12 21:20:46 +0530

    Co-oridinate description fixed

--- Passenger.java
+++ Passenger.java
@@ -9,10 +9,7 @@ public class Passenger {
 	double oriX, oriY;
 	double destX, destY;
 	String name;
-	int oria = 0; 
-	int orib = 0;
-	int desa = 0;
-	int desb = 0;
+	BinNumber ori,dest;
 	
 
 void createPassenger() throws IOException {
@@ -28,13 +25,22 @@ void createPassenger() throws IOException {
 BinNumber allocation() {
 	
 		BinNumber n = new BinNumber();
+		BinNumber m = new BinNumber();
 		if(oriX>22.454 && oriX<22.657 && oriY>88.281 && oriY<88.491) {
 			n.a = (int) Math.floor((22.657-oriX)/0.00655);
 			n.b = (int) Math.floor((88.491-oriY)/0.00954);
-			oria = n.a;
-			orib = n.b;
+			ori.a = n.a;
+			ori.b = n.b;
 			
 		}
+		if(destX>22.454 && destX<22.657 && destY>88.281 && destY<88.491) {
+			m.a = (int) Math.floor((22.657-destX)/0.00655);
+			m.b = (int) Math.floor((88.491-destY)/0.00954);
+			dest.a = m.a;
+			dest.b = m.b;
+			
+		}
+		
 		return n;
 		
 	}
