af504a0a5f5392213a0d3dd6381f433c46fc5ed3
Raj <30487563+bittu511@users.noreply.github.com>
2018-05-14 11:31:32 +0530

    Update Path.java

--- Path.java
+++ Path.java
@@ -1,8 +1,8 @@
-package carpool;
+package carpool_master1;
 
 import java.util.ArrayList;
 
 public class Path {
-	ArrayList <BinNumber> binno;
+	ArrayList <BinNumber> binno = new ArrayList <BinNumber>();
 	
 }
