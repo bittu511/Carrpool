b4b1125817ebd45795b283bd805d001dc5db25f0
Raj <30487563+bittu511@users.noreply.github.com>
2018-05-23 13:38:27 +0530

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,15 +1,22 @@
-package carpool;
+package carpool_master1;
 
 import java.util.ArrayList;
 
 public class Driver {
-	double x;
-	double y;
 	Passenger p1;
-	ArrayList <Passenger> plist = new ArrayList <Passenger>();
+	ArrayList<Passenger> plist = new ArrayList<Passenger>();
 	int pcount = 0;
-	
+
 	BinNumber dbin = new BinNumber();
-	 // driver current bin
-	
+
+	// driver current bin
+	Driver() {
+
+	}
+
+	Driver(Driver d) {
+		p1 = d.p1;
+		plist = (ArrayList<Passenger>) d.plist.clone();
+		pcount = d.pcount;
+	}
 }
